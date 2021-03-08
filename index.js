32e270b84a582d0ea503affe071362e3bebd5764
Glauber Noccioli <gnoccioli@gmail.com>
2021-03-08 00:20:12 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+//Aluno: Glauber Noccioli de Souza
+//Curso: Defesa Cibernética
+
+//início do loop para repetir a operação caso o usuário solicite
+for(;;){
+  console.clear()
+  alert('\n\nInforme os valores praticados para venda de Etanol e Gasolina. Entradas inválidas encerrarão o programa.')
+  
+  //inserção dos dados
+	  etanol = prompt('\n\nQual é o valor do Etanol:')
+	  gasolina = prompt('\n\nQual é o valor da Gasolina:')
+
+  //validação dos dados inseridos
+	  valor_e = parseFloat(etanol.replace(",","."))
+	  valor_g = parseFloat(gasolina.replace(",","."))
+
+	  if ((isNaN(valor_e+valor_g)) || (valor_e <= 0) || (valor_g <= 0)) break;
+
+  //cálculo 
+	  if ( (valor_e / valor_g) == 0.7 )
+		{alert('\n\nTanto faz Etanol ou Gasolina. O custo por Km rodado será o mesmo.')}
+	  else {
+		vantagem = (valor_e/valor_g > 0.7 ? 'gasolina':'etanol')
+		alert('\n\nÉ mais vantajoso abastecer com ' +  vantagem)
+	  }
+
+  repete = prompt('\n\nDeseja fazer um novo cálculo ? (digite sim para repetir)')
+if (repete!='sim') break;}
