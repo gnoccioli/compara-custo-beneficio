53a61456d4991e06a80a27b2bf5a4695e15f213b
gnoccioli <47876602+gnoccioli@users.noreply.github.com>
2021-03-07 23:39:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,27 +3,24 @@
 
 //início do loop para repetir a operação caso o usuário solicite
 for(;;){
-  console.clear()
-  alert('\n\nInforme os valores praticados para venda de Etanol e Gasolina. Entradas inválidas encerrarão o programa.')
+  console.clear();
+  alert('\n\nInforme os valores praticados para venda de Etanol e Gasolina. Entradas inválidas encerrarão o programa.');
   
   //inserção dos dados
-	  etanol = prompt('\n\nQual é o valor do Etanol:')
-	  gasolina = prompt('\n\nQual é o valor da Gasolina:')
+	etanol = prompt('\n\nQual é o valor do Etanol:');
+	gasolina = prompt('\n\nQual é o valor da Gasolina:');
 
   //validação dos dados inseridos
-	  valor_e = parseFloat(etanol.replace(",","."))
-	  valor_g = parseFloat(gasolina.replace(",","."))
+	valor_e = parseFloat(etanol.replace(",","."));
+	valor_g = parseFloat(gasolina.replace(",","."));
 
-	  if ((isNaN(valor_e+valor_g)) || (valor_e <= 0) || (valor_g <= 0)) break;
+	if ((isNaN(valor_e+valor_g)) || (valor_e <= 0) || (valor_g <= 0)) break;
 
   //cálculo 
-	  if ((valor_e / valor_g) == 0.7 )
+	if ((valor_e / valor_g) == 0.7)
 		{alert('\n\nTanto faz Etanol ou Gasolina. O custo por Km rodado será o mesmo.')}
-	  else {
-		vantagem = (valor_e/valor_g > 0.7 ? 'Gasolina':'Etanol')
-		alert('\n\nÉ mais vantajoso abastecer com ' +  vantagem)
-	  }
+	else {alert('\n\nÉ mais vantajoso abastecer com ' + (valor_e/valor_g > 0.7 ? 'Gasolina':'Etanol'))};
   
   //fechamento do loop de repetição
-  repete = prompt('\n\nDeseja fazer um novo cálculo ? (digite sim para repetir)')
-if (repete!='sim') break;}
+  repete = prompt('\n\nDeseja fazer um novo cálculo ? (digite sim para repetir)');
+if (repete!='sim') break;};
